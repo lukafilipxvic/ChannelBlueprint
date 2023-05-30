9d382d5a077b4b260c8aadf7bad00f90f6a9d01f
Luka Filipović <lukafilip.vic@gmail.com>
2023-05-30 13:26:15 +1000

    layout new

--- Home.py
+++ Home.py
@@ -1,22 +1,12 @@
 import streamlit as st
-from pytrends.request import TrendReq
+import pandas as pd
 
 st.set_page_config(
     layout="centered",
-    page_title="ChannelBlueprint",
+    page_title="Channel Blueprint",
     page_icon="📺",
 )
 
-st.sidebar.success("Select a page above.")
-
 # Heading of App
 st.write("# Channel Blueprint 📺")
-
-# Get the three pillars from the user
-pillar1 = st.text_input("Enter Pillar 1:")
-pillar2 = st.text_input("Enter Pillar 2:")
-pillar3 = st.text_input("Enter Pillar 3:")
-
-# Create a pytrends object
-pytrends = TrendReq()
-
+st.write("A Streamlit app to accelerate your Youtube channel development.")
---
+++ pages/1_📄_Transcript_Downloader.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.formatters import TextFormatter
+
+st.set_page_config(
+    layout="centered",
+    page_title="Transcript Downloader",
+    page_icon="📄",
+)
+
+# Heading of App
+st.write("# Transcript Downloader 📄")
+
+# Type in video URL
+video_id = st.text_input("Enter Youtube video ID")
+
+if "https://" in video_id or "youtu" in video_id:
+    st.warning("Please enter video ID only")
+
+if st.button("Get Transcript"):
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+
+    formatter = TextFormatter()
+
+    # .format_transcript(transcript) turns the transcript into a JSON string.
+    txt_transcript = formatter.format_transcript(transcript)
+
+    # Columns to sort informative info annd download button
+    col1, col2, col3 = st.columns([3,1,1], gap="small")
+    # Download txt_transcript as a txt file
+    col1.download_button(
+        label="Download Transcript as .txt file",
+        data=txt_transcript.encode('utf-8'),
+        file_name="transcript.txt",
+        mime="text/plain")
+    
+    # Count the number of words in txt_transcript
+    word_count = len(txt_transcript.split())
+    col2.write(f'Word count: {word_count}')
+
+    # Print text formatted transcript
+    st.write(txt_transcript)
